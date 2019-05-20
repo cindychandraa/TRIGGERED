73a707fbe5e37e697ff0644e10e28ee68942bd86
Cindy Chandra <cxndych@gmail.com>
2019-05-20 11:06:01 +0700

    Initial Commit

---
+++ Triggered/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Triggered
+//
+//  Created by Cindy Chandra on 20/05/19.
+//  Copyright © 2019 Cindy Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
